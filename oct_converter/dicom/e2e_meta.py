01fd0ed4bc8231d45aa81e4f7378b5c4a5631b45
Rafay Shehzad Syed <rafaysyed0014@gmail.com>
2026-08-13 16:04:07 +0500

    Add files via upload

---
+++ oct_converter/dicom/e2e_meta.py
@@ -0,0 +1,173 @@
+from __future__ import annotations
+
+from oct_converter.dicom.metadata import (
+    DicomMetadata,
+    ImageGeometry,
+    ManufacturerMeta,
+    OCTDetectorType,
+    OCTImageParams,
+    OPTAcquisitionDevice,
+    OPTAnatomyStructure,
+    PatientMeta,
+    SeriesMeta,
+)
+from oct_converter.image_types import FundusImageWithMetaData, OCTVolumeWithMetaData
+
+
+def e2e_patient_meta(meta: dict) -> PatientMeta:
+    """Creates PatientMeta from e2e info stored in raw metadata
+
+    Args:
+        meta: Nested dictionary of metadata accumulated by the E2E reader
+    Returns:
+        PatientMeta: Patient metadata populated by oct
+    """
+    patient = PatientMeta()
+
+    patient_data = meta.get("patient_data")
+    if patient_data:
+        # Heidelberg's updated anonymization process wipes
+        # this section of metadata
+        patient.first_name = patient_data[0].get("first_name")
+        patient.last_name = patient_data[0].get("surname")
+        patient.patient_id = patient_data[0].get("patient_id")
+        patient.patient_sex = patient_data[0].get("sex")
+        # TODO patient.patient_dob
+        # Currently, E2E's patient_dob is incorrect, see
+        # the E2E reader for more context.
+
+    return patient
+
+
+def e2e_series_meta(id, laterality, acquisition_date, metadata) -> SeriesMeta:
+    """Creates SeriesMeta from info parsed by the E2E reader
+
+    Args:
+        id: Equivalent to oct.volume_id or fundus.image_id
+        laterality: R or L, from image.laterality
+        acquisition_date: Scan date for OCT, or None for fundus
+        metadata: Additional metadata
+    Returns:
+        SeriesMeta: Series metadata populated by oct
+    """
+    split = id.split("_")
+    patient_db_id, study_id, series_id = split[0], split[1], split[2]
+    series = SeriesMeta()
+
+    series.study_id = study_id
+    series.series_id = series_id
+    series.laterality = laterality
+    series.acquisition_date = acquisition_date
+    series.opt_anatomy = OPTAnatomyStructure.Retina
+    if metadata.get("examined_structure", {}).get(id):
+        structure = metadata["examined_structure"][id]
+        try:
+            series.opt_anatomy = getattr(OPTAnatomyStructure, structure)
+        except AttributeError:
+            series.opt_anatomy = OPTAnatomyStructure.Unspecified
+    if metadata.get("enface_modality", {}).get(id):
+        series.protocol = metadata["enface_modality"][id]
+    if metadata.get("scan_pattern", {}).get(id):
+        series.description = metadata["scan_pattern"][id]
+
+    return series
+
+
+def e2e_manu_meta() -> ManufacturerMeta:
+    """Creates ManufacturerMeta with Heidelberg defaults.
+
+    Args:
+        None
+    Returns:
+        ManufacturerMeta: Manufacture metadata module
+    """
+    manufacture = ManufacturerMeta()
+
+    manufacture.manufacturer = "Heidelberg Engineering"
+    manufacture.manufacturer_model = "Spectralis"
+    manufacture.device_serial = ""
+    manufacture.software_version = ""
+
+    return manufacture
+
+
+def e2e_image_geom(pixel_spacing: list) -> ImageGeometry:
+    """Creates ImageGeometry from E2E metadata
+
+    Args:
+        pixel_spacing: Pixel spacing identified by E2E reader
+    Returns:
+        ImageGeometry: Geometry data populated by pixel_spacing
+    """
+    image_geom = ImageGeometry()
+    image_geom.pixel_spacing = [pixel_spacing[1], pixel_spacing[0]]
+    if len(pixel_spacing) == 3:
+        image_geom.slice_thickness = pixel_spacing[2]
+    image_geom.image_orientation = [1, 0, 0, 0, 1, 0]
+
+    return image_geom
+
+
+def e2e_image_params() -> OCTImageParams:
+    """Creates OCTImageParams specific to E2E
+
+    Args:
+        None
+    Returns:
+        OCTImageParams: Image params populated with E2E defaults
+    """
+    image_params = OCTImageParams()
+    image_params.opt_acquisition_device = OPTAcquisitionDevice.OCTScanner
+    image_params.DetectorType = OCTDetectorType.CCD
+    image_params.IlluminationWaveLength = 880
+    image_params.IlluminationPower = 1200
+    image_params.IlluminationBandwidth = 50
+    image_params.DepthSpatialResolution = 7
+    image_params.MaximumDepthDistortion = 0.5
+    image_params.AlongscanSpatialResolution = 13
+    image_params.MaximumAlongscanDistortion = 0.5
+    image_params.AcrossscanSpatialResolution = 13
+    image_params.MaximumAcrossscanDistortion = 0.5
+
+    return image_params
+
+
+def e2e_dicom_metadata(
+    image: FundusImageWithMetaData | OCTVolumeWithMetaData,
+) -> DicomMetadata:
+    """Creates DicomMetadata for oct or fundus image and populates each module
+
+    Args:
+        image: Oct or Fundus image type created by the E2E reader
+    Returns:
+        DicomMetadata: Populated DicomMetadata created with fundus or oct metadata
+    """
+
+    patient_info = e2e_patient_meta(image.metadata)
+    manufacturer_info = e2e_manu_meta()
+    oct_image_params = e2e_image_params()
+
+    if isinstance(image, OCTVolumeWithMetaData):
+        series_info = e2e_series_meta(
+            image.volume_id,
+            image.laterality,
+            image.acquisition_date,
+            image.metadata,
+        )
+        image_geometry = e2e_image_geom(image.pixel_spacing)
+    else:
+        series_info = e2e_series_meta(
+            image.image_id,
+            image.laterality,
+            image.acquisition_date,
+            image.metadata,
+        )
+        image_geometry = e2e_image_geom(image.pixel_spacing)
+
+    return DicomMetadata(
+        patient_info=patient_info,
+        series_info=series_info,
+        manufacturer_info=manufacturer_info,
+        image_geometry=image_geometry,
+        oct_image_params=oct_image_params,
+    )
---
+++ oct_converter/readers/binary_structs/e2e_binary.py
@@ -0,0 +1,227 @@
+from construct import (
+    Array,
+    Bytes,
+    ExprAdapter,
+    Float32l,
+    Float64l,
+    Int8un,
+    Int16un,
+    Int32sn,
+    Int32un,
+    Int64un,
+    PaddedString,
+    Struct,
+    this,
+)
+
+
+# PaddedString only supports ASCII; Latin-1 is needed for names with ä, ö, å etc.
+def Latin1String(n):
+    return ExprAdapter(
+        Bytes(n),
+        lambda obj, _: obj.rstrip(b"\x00").decode("latin-1"),
+        lambda obj, _: obj.encode("latin-1").ljust(n, b"\x00")[:n],
+    )
+
+
+# Mostly based on description of .e2e file format here:
+#         https://bitbucket.org/uocte/uocte/wiki/Heidelberg%20File%20Format.
+
+header_structure = Struct(
+    "magic1" / PaddedString(12, "ascii"),
+    "version" / Int32un,
+    "unknown" / Array(10, Int16un),
+)
+main_directory_structure = Struct(
+    "magic2" / PaddedString(12, "ascii"),
+    "version" / Int32un,
+    "unknown" / Array(10, Int16un),
+    "num_entries" / Int32un,
+    "current" / Int32un,
+    "prev" / Int32un,
+    "unknown3" / Int32un,
+)
+sub_directory_structure = Struct(
+    "pos" / Int32un,
+    "start" / Int32un,
+    "size" / Int32un,
+    "unknown" / Int32un,
+    # Patient DB ID is set by the software
+    # and is not necessarily equal to patient_id_structure's patient_id
+    "patient_db_id" / Int32un,
+    "study_id" / Int32un,
+    "series_id" / Int32un,
+    "slice_id" / Int32sn,
+    "unknown2" / Int16un,
+    "unknown3" / Int16un,
+    "type" / Int32un,
+    "unknown4" / Int32un,
+)
+chunk_structure = Struct(
+    "magic3" / PaddedString(12, "ascii"),
+    "unknown" / Int32un,
+    "unknown2" / Int32un,
+    "pos" / Int32un,
+    "size" / Int32un,
+    "unknown3" / Int32un,
+    # Patient DB ID is set by the software
+    # and is not necessarily equal to patient_id_structure's patient_id
+    "patient_db_id" / Int32un,
+    "study_id" / Int32un,
+    "series_id" / Int32un,
+    "slice_id" / Int32sn,
+    "ind" / Int16un,
+    "unknown4" / Int16un,
+    "type" / Int32un,
+    "unknown5" / Int32un,
+)
+image_structure = Struct(
+    "size" / Int32un,
+    "type" / Int32un,
+    "unknown" / Int32un,
+    "height" / Int32un,
+    "width" / Int32un,
+)
+patient_id_structure = Struct(
+    "first_name" / Latin1String(31),
+    "surname" / Latin1String(51),
+    "title" / Latin1String(15),
+    "birthdate" / Int32un,
+    "sex" / PaddedString(1, "ascii"),
+    "patient_id" / Latin1String(25),
+)
+lat_structure = Struct(
+    "unknown" / Array(14, Int8un),
+    "laterality" / PaddedString(1, "ascii"),
+    "unknown2" / Int8un,
+)
+contour_structure = Struct(
+    "unknown0" / Int32un,
+    "id" / Int32un,
+    "unknown1" / Int32un,
+    "width" / Int32un,
+)
+
+# following the spec from
+# https://github.com/neurodial/LibE2E/blob/d26d2d9db64c5f765c0241ecc22177bb0c440c87/E2E/dataelements/bscanmetadataelement.cpp#L75
+bscan_metadata = Struct(
+    "unknown1" / Int32un,
+    "imgSizeY" / Int32un,
+    "imgSizeX" / Int32un,
+    "posX1" / Float32l,
+    "posY1" / Float32l,
+    "posX2" / Float32l,
+    "posY2" / Float32l,
+    "zero1" / Int32un,
+    "unknown2" / Float32l,
+    "scaley" / Float32l,
+    "unknown3" / Float32l,
+    "zero2" / Int32un,
+    "unknown4" / Array(2, Float32l),
+    "zero3" / Int32un,
+    "imgSizeWidth" / Int32un,
+    "numImages" / Int32un,
+    "aktImage" / Int32un,
+    "scanType" / Int32un,
+    "centrePosX" / Float32l,
+    "centrePosY" / Float32l,
+    "unknown5" / Int32un,
+    "acquisitionTime" / Int64un,
+    "numAve" / Int32un,
+    "imgQuality" / Float32l,
+)
+
+# Chunk 7: Eye Data (libE2E)
+eye_data = Struct(
+    "eyeSide" / PaddedString(1, "ascii"),
+    "iop_mmHg" / Float64l,
+    "refraction_dpt" / Float64l,
+    "c_curve_mm" / Float64l,
+    "vfieldMean" / Float64l,
+    "vfieldVar" / Float64l,
+    "cylinder_dpt" / Float64l,
+    "axis_deg" / Float64l,
+    "correctiveLens" / Int16un,
+    "pupilSize_mm" / Float64l,
+)
+
+# 9001 Device Name
+# Files examined have n_strings=3, string_size=256,
+# text=["Heidelberg Retina Angiograph", "HRA", ""]
+device_name = Struct(
+    "n_strings" / Int32un,
+    "string_size" / Int32un,
+    "text" / Array(this.n_strings, PaddedString(this.string_size, "u16")),
+)
+
+# 9005 Examined Structure
+# Files examined have n_strings=1, string_size=256,
+# text=["Retina"]
+examined_structure = Struct(
+    "n_strings" / Int32un,
+    "string_size" / Int32un,
+    "text" / Array(this.n_strings, PaddedString(this.string_size, "u16")),
+)
+
+# 9006 Scan Pattern
+# Files examined have n_strings=2, string_size=256,
+# and scan patterns including "OCT Art Volume", "Images", "OCT B-SCAN",
+# "3D Volume", "OCT Star Scan"
+scan_pattern = Struct(
+    "n_strings" / Int32un,
+    "string_size" / Int32un,
+    "text" / Array(this.n_strings, PaddedString(this.string_size, "u16")),
+)
+
+# 9007 Enface Modality
+# Files examined have n_strings=2, string_size=256,
+# and modalities including ["Infra-Red", "IR"],
+# ["Fluroescein Angiography", "FA"], ["ICG Angiography", "ICGA"]
+enface_modality = Struct(
+    "n_strings" / Int32un,
+    "string_size" / Int32un,
+    "text" / Array(this.n_strings, PaddedString(this.string_size, "u16")),
+)
+
+# 9008 OCT Modality
+# Files examined have n_strings=2, string_size=256, text=["OCT", "OCT"]
+oct_modality = Struct(
+    "n_strings" / Int32un,
+    "string_size" / Int32un,
+    "text" / Array(this.n_strings, PaddedString(this.string_size, "u16")),
+)
+
+# 10025 Localizer
+# From eyepy; "transform" is described as "Parameters of affine transformation"
+localizer = Struct(
+    "unknown" / Array(6, Float32l),
+    "windate" / Int32un,
+    "transform" / Array(6, Float32l),
+)
+
+# 3 seems to indicate the start of the chunk pattern
+# Examined files seem to have a mostly-regular pattern of 3, 2, ..., 5, 39
+# Both chunks 3 and 5 seem to include laterality info
+pre_data = Struct(
+    "unknown" / Int32un,
+    "laterality" / PaddedString(1, "ascii"),
+    # There's more here that I'm unsure of.
+    # There seems to be an "ART" in this chunk.
+)
+
+# 39 has some time zone data
+time_data = Struct(
+    "unknown" / Array(46, Int32un),
+    "timezone1" / PaddedString(66, "u16"),
+    "unknown2" / Array(9, Int16un),
+    "timezone2" / PaddedString(66, "u16"),
+    # There's more in this chunk (possibly datetimes, given tz)
+    # and the chunk size varies.
+)
+
+# 52, 54, 1000, 1001 seem to be UIDs with padded strings
+# 1000 may be StudyInstanceUID
+uid_data = Struct("uid" / PaddedString(64, "ascii"))
+
+# 1007 padded string with a brand name
+unknown_data = Struct("unknown" / PaddedString(64, "ascii"))
